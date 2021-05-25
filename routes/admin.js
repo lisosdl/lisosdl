079660bae218fd5dd094f4f10b6e3cd274f1089c
lisosdl <vqldwjdghks@naver.com>
2021-05-25 18:12:14 +0900

    관리자 이력서 양식 작업

--- routes/admin.js
+++ routes/admin.js
@@ -6,8 +6,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	const 
-	addClass : '',
+	const params = {
+		addClass : 'admin_page',
+	};
+	
 	return res.render("admin/main");
 });
 
