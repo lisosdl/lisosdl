578c317a062f6c3c9957df75e49e35e44ae63c2c
lisosdl <vqldwjdghks@naver.com>
2021-05-25 12:21:53 +0900

    router 구성, 기본 템플릿 설정

--- routes/member.js
+++ routes/member.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.route('/join')
 		// 회원가입 양식
 		.get((req, res, next) => {
-			
+			res.render('member/form');
 		})
 		// 회원가입 처리
 		.post((req, res, next) => {
