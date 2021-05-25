43c4f55bf4dddd6c716b1f6dd5e6f19d907b2958
lisosdl <vqldwjdghks@naver.com>
2021-05-25 17:11:31 +0900

    관리자 페이지 기본 스타일 작업

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	return res.send("관리자 페이지");
+	const 
+	addClass : '',
+	return res.render("admin/main");
 });
 
 module.exports = router;
